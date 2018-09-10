b6433c7190b76f10de1f20273624feac7d10ffdd
Zach <zdelrosario@outlook.com>
2018-09-10 11:39:48 -0700

    initial (real) commit

---
+++ R/matched_inner.R
@@ -0,0 +1,48 @@
+#' Do a batch of inner products on a data frame
+#'
+#' @param df_weights inner product weights
+#' @param df_target target data frame
+#' @return data frame with inner product results
+#' @export
+#' @examples
+#' df_weights <-
+#'   tibble(
+#'     out_name = c("Y1"),
+#'     X1 = c(0.5),
+#'     X2 = c(-0.5)
+#'   )
+#' df_target <-
+#'  tibble(
+#'    X1 = c( 0, 1, 2, 3),
+#'    X2 = c( 0, 0, 1, 1)
+#'  )
+#'
+#' matched_inner(df_weights, df_target)
+
+matched_inner <- function(df_weights, df_target) {
+  ## Check input invariants
+  if (!all(names(df_weights %>% dplyr::select(-out_name)) %in% names(df_target))) {
+    stop("df_weights contains targets variables not found in df_target")
+  }
+  if (!("out_name" %in% names(df_weights))) {
+    stop("df_weights does not contain an out_name column")
+  }
+  ## Form matrices
+  W <-
+    df_weights %>%
+    dplyr::select(-out_name) %>%
+    as.matrix()
+  M <-
+    df_target %>%
+    dplyr::select(names(df_weights %>% select(-out_name))) %>%
+    as.matrix()
+
+  ## Form result
+  res <- matrix(
+    M %*% t(W),
+    nrow = dim(M)[1],
+    ncol = dim(W)[1]
+  )
+  colnames(res) <- df_weights %>% pull(out_name)
+  as.tibble(res)
+}
---
+++ R/pi_projection.R
@@ -0,0 +1,40 @@
+#' Generate a projection to the pi-subspace
+#'
+#' @param df_dim dimension matrix
+#' @return data frame for matched_inner()
+#' @export
+#' @examples
+#' df_dim <-
+#'   tribble(
+#'     ~rho, ~U, ~d, ~mu,
+#'        1,  0,  0,   1,
+#'       -3,  1,  1,  -1,
+#'        0, -1,  0,  -1
+#'   )
+#'
+#' df_pi_proj <-
+#'   pi_projection(df_dim)
+#'
+#' df_target <-
+#'   tribble(
+#'     ~rho, ~U, ~d, ~mu,
+#'      1.2, 10,  5, 1e-5,
+#'      1.2, 20,  5, 1e-5,
+#'      1.2, 10, 10, 1e-5,
+#'      1.2, 20, 10, 1e-5
+#'   )
+#'
+#' matched_inner(df_pi_proj, df_target)
+
+pi_projection <- function(df_dim) {
+  ## Compute basis
+  D <- as.matrix(df_dim)
+  res <- qr(t(D))
+  rank <- res$rank
+  V <- t(qr.Q(res, complete = TRUE)[, -(1:rank)])
+  colnames(V) <- names(df_dim)
+
+  as.tibble(V) %>%
+    dplyr::mutate(out_name = stringr::str_c("pi", 1:dim(V)[1])) %>%
+    dplyr::select(out_name, tidyselect::everything())
+}
